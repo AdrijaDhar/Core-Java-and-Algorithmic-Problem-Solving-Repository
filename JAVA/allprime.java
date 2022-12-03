0b290658fa3b2a64c4128dae5cca88636912b2d1
AdrijaDhar2003 <96368997+AdrijaDhar2003@users.noreply.github.com>
2022-12-03 10:31:46 +0530

    commit

---
+++ JAVA/allprime.java
@@ -0,0 +1,21 @@
+package JAVA;
+import java.util.Scanner;
+public class allprime {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int i=1;
+        for(i=2;i<=n;i++){
+			int prime=1;
+			for(int j=2;j<=i/2;j++){
+				if(i%j==0){
+					prime=0;
+					break;
+				}
+			}
+			if(prime==1){
+				System.out.println(i);
+			}
+		}
+    }
+}
---
+++ JAVA/codeforcesk.java
@@ -0,0 +1,17 @@
+package JAVA;
+import java.util.Scanner;
+public class codeforcesk {
+    public static void main(String[] args) {
+        
+    
+    Scanner s=new Scanner(System.in);
+    int t=s.nextInt();
+    for(int i=0;i<t;i++){
+     int n=s.nextInt();
+     
+    }
+
+    
+}
+ }
+
