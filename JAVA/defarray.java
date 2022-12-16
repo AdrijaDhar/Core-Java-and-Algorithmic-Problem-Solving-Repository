c82bdeea49d48e0af944d7b3badeeb9850d11f32
AdrijaDhar2003 <96368997+AdrijaDhar2003@users.noreply.github.com>
2022-12-16 10:53:27 +0530

    git committed

---
+++ JAVA/defarray.java
@@ -0,0 +1,12 @@
+package JAVA;
+
+public class defarray {
+    public static void main(String[] args) {
+        int arr[]=new int[10];
+        System.out.println(arr[0]);
+        char cArr[]=new char[10];
+        System.out.println(cArr[0]);
+         double dArr[]=new double[10];
+        System.out.println(dArr[0]);
+    }
+}
